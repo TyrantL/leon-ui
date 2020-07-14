8b74a59fa5e9ea4facf12b8db2e725be180e603f
leon <lvzzh.leon@gmail.com>
2020-07-14 14:58:26 +0800

    add release

--- build/release.js
+++ build/release.js
@@ -2,3 +2,34 @@
  * @author: leon
  * @date: 2020/7/14 2:47 下午
  */
+const { exec } = require('shelljs');
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'selected',
+      message: '请选择版本升级类型',
+      choices: [
+        'major',
+        {
+          name: '大版本更新',
+          disabled: '较大版本更新时选择此项',
+        },
+        'minor',
+        {
+          name: '小版本更新',
+          disabled: '较小版本更新时选择此项',
+        },
+        'patch',
+        {
+          name: '更新补丁',
+          disabled: '修复bug选择此项',
+        },
+      ],
+    },
+  ])
+  .then(answer => {
+    exec(`npm version ${answer.selected} && npm run lib`);
+  });
